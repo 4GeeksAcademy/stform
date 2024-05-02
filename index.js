5d037d685c466f7b27622aad173196f62f2ab28f
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-05-02 15:05:44 +0000

    nuevost

---
+++ index.js
@@ -0,0 +1,4 @@
+const temaOscuro = () => {
+    document.querySelector("html").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#myIcon").setAttribute("class", "bi bi-sun-fill");
+}
