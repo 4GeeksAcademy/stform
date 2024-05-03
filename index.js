0f254b1274437c03c5a02dd09c459bd46c0b8b56
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-05-03 14:33:50 +0000

    actualizado

--- index.js
+++
@@ -1,4 +0,0 @@
-const temaOscuro = () => {
-    document.querySelector("html").setAttribute("data-bs-theme", "dark");
-    document.querySelector("#myIcon").setAttribute("class", "bi bi-sun-fill");
-}
